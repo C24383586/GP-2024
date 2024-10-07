d220dae241c73b706cf0e9614930e68967d64feb
C24383586 Omar Aglan <C24383586@myTUDublin.ie>
2024-10-07 16:52:25 +0100

    I made a shooter game.

--- the-course/player.gd
+++ the-course/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var bullet_scene:PackedScene
-@export var bullet_spawn_point:Node2D
+@export var bullet_spawn:Node2D
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
@@ -18,13 +18,27 @@ func _physics_process(delta: float) -> void:
 	
 	var f = Input.get_axis("move_backwards", "move_forwards")
 
+	var can_fire = true;
+
 	var vel = transform.y * f * SPEED
 	print(transform.y)
 	velocity = vel
-	
-	if Input.is_action_pressed("fire"):
-		var b = bullet_scene.instantiate()
-		b.global_position = bullet_spawn_point.global_position
-		get_parent().add_child(b)
-	
 	move_and_slide()
+	
+	
+	if Input.is_action_pressed("fire") and can_fire:
+		var b = bullet_scene.instantiate()
+		b.global_position = bullet_spawn.global_position
+		b.global_rotation = bullet_spawn.global_rotation
+		get_tree().get_root().add_child(b) 
+		can_fire = false
+		$Timer.start()
+	
+	pass
+	
+
+func _on_timer_timeout():
+	var can_fire = true
+	pass # Replace with function body.
+	
+	
